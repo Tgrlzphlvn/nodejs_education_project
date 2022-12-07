01b99f523db5f9c180ac6dd0afe817c7b64354c4
Tuğrul Özpehlivan <ozpehlivan.tgrl@gmail.com>
2022-12-07 14:06:17 +0300

    ejs entegre edildi

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
+//* Template Engine
+app.set('view engine', 'ejs');
+
+//* Middlewares
+app.use(express.static("public"));
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).send('Index Page');
+//* Routes
+app.get("/", (req, res) => {
+  res.status(200).render("index", {
+    page_name: "index"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).render("about", {
+    page_name: "about"
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
