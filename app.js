725f15086e91476e59047d62cfb0f5908d48ec79
Tuğrul Özpehlivan <ozpehlivan.tgrl@gmail.com>
2022-12-09 22:12:38 +0300

    Roller belirlendi

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
 const pageRoute = require("./routes/pageRoute");
 const courseRoute = require("./routes/courseRoute");
 const categoryRoute = require("./routes/categoryRoute");
+const userRoute = require("./routes/userRoute");
 
 const app = express();
 
@@ -15,16 +19,32 @@ mongoose
 //* Template Engine
 app.set("view engine", "ejs");
 
+//* Global variables
+global.userIN = null;
+
 //* Middlewares
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "my_keyboard_cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: "mongodb://localhost/smartedu-db" }),
+  })
+);
 
 //* Routes
+app.use("*", (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 app.use("/", pageRoute);
 app.use("/about", pageRoute);
 app.use("/courses", courseRoute);
 app.use("/categories", categoryRoute);
+app.use("/users", userRoute);
 
 const port = 3000;
 
